427ba389062e3845bbd364e258dc6d3e3eaf3219
marcang0803 <marcang0802@gmail.com>
2026-03-19 14:11:13 +0800

    Testing

---
+++ api_ob.py
@@ -0,0 +1,7 @@
+class stripe_api:
+    def __init__(self, api_key):
+        self.api_key = ''
+
+    def get_api(self):
+        return self.api_key
+    
--- payment.py
+++ payment.py
@@ -1,4 +1,19 @@
 import stripe
+import time
+import api_ob
 from idem_key import generate_key
+session_id = ''
+
+def session_time():
+    # sets session time and generate idem key
+    start = time.time()
+    session_id = generate_key
+
+    return start
+
+def provide_payment():
+    pass
 
 
+
+print(session_time())
