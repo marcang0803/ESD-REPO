44ec9102a69ae91d9df47a7b8f2fc837c1349f64
marcang0803 <marcang0802@gmail.com>
2026-03-19 14:33:45 +0800

    rename api_ob

--- payment.py
+++ payment.py
@@ -1,14 +1,14 @@
 import stripe
 import time
-import api_ob
+import api_key
 from idem_key import generate_key
 session_id = ''
 
 def session_time():
-    # sets session time and generate idem key
+    # sets session time
+    
     start = time.time()
-    session_id = generate_key
-
+    
     return start
 
 def provide_payment():
