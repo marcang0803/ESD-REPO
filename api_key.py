5cef01912ef64cc66d0a85e7ac1da62da24c2c00
marcang0803 <marcang0802@gmail.com>
2026-03-21 18:33:06 +0800

    Updated api_key.py

--- api_key.py
+++ api_key.py
@@ -1,7 +1,17 @@
-class stripe_api:
+class StripeAPI:
     def __init__(self, api_key):
-        self.api_key = ''
+        # We use a leading underscore to signal "internal use"
+        self._api_key = api_key
 
-    def get_api(self):
-        return self.api_key
+    @property
+    def api_key(self):
+        """The getter: allows you to call client.api_key"""
+        return self._api_key
+
+    @api_key.setter
+    def api_key(self, value):
+        """The setter: allows you to add validation logic"""
+        if not value.startswith("sk_"):
+            raise ValueError("Invalid Stripe API key format.")
+        self._api_key = value
     
--- session_token.py
+++ session_token.py
@@ -1,2 +1,18 @@
 from idem_key import generate_key
 import time
+
+start = 0
+key = ''
+
+def get_current_time():
+    return time.time()
+
+def initiate_token():
+    cur = get_current_time
+    if (start == 0):
+        # get current time in seconds from epoch, start time for session under this user
+        start = time.time()
+        key = generate_key
+        return (start, key)
+    else:
+
