a1505aa442272671521f7edac9c4e26d67c28f7e
marcang0803 <marcang0802@gmail.com>
2026-03-23 04:30:46 +0800

    trialing

--- api_key.py
+++ api_key.py
@@ -1,16 +1,16 @@
 class StripeAPI:
     def __init__(self, api_key):
-        # We use a leading underscore to signal "internal use"
+        
         self._api_key = api_key
 
     @property
     def api_key(self):
-        """The getter: allows you to call client.api_key"""
+        # Calls api_key
         return self._api_key
 
     @api_key.setter
     def api_key(self, value):
-        """The setter: allows you to add validation logic"""
+        # Set api_key
         if not value.startswith("sk_"):
             raise ValueError("Invalid Stripe API key format.")
         self._api_key = value
