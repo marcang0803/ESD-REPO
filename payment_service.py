d99b82bdb4ab9320b9e3efc963e924b1efd192c2
marcang0803 <marcang0802@gmail.com>
2026-03-23 23:56:49 +0800

    added code in payment_service

--- payment_service.py
+++ payment_service.py
@@ -5,9 +5,38 @@ from api_key import StripeAPI
 app = Flask(__name__)
 stripe_config = StripeAPI()
 
-def provide_payment():
-    pass
+@app.route('/process_payout', methods=['POST'])
+
+def provide_payout():
+    data = request.get_json()
+
+    #details require database extract
+    provider_account = data.get('provider_account')
+    amount = data.get('amount') 
+    idempt_key = data.get('idem_key') #Link to session_token.py
+
+    try:
+        stripe.api_key = stripe_config.api_key
+        # USE idem_key to prevent double-charging/double-payouts
+        transfer = stripe.Transfer.create(
+            amount=int(amount * 100),
+            currency="sgd",
+            destination=provider_account,
+            idempotency_key=idempt_key
+        )
+        # returning status via json format
+        return jsonify ({
+            "status": "success",
+            "transfer_id": transfer.id,
+            "message": "Payout successful"
+        }), 200
+    
+    except stripe.error.StripeError as e:
+        return jsonify({"status": "fail", "error": str(e)}), 400
+    
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True) # Note! Check port number before deployment
+    
 
 
-
-print(session_time())
