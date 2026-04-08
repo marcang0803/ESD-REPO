a013e7c9fd022c2fec456f4ed0e9c798d75abddd
marcang0803 <marcang0802@gmail.com>
2026-04-08 13:57:18 +0800

    KMN

--- class-service/db.py
+++ class-service/db.py
@@ -2,19 +2,25 @@ import mysql.connector
 from mysql.connector import pooling
 import os
 
-db_config = {
-    "host": os.environ.get("DB_HOST", "localhost"),
-    "port": int(os.environ.get("DB_PORT", 3306)),
-    "user": os.environ.get("DB_USER", "root"),
-    "password": os.environ.get("DB_PASSWORD", "password"),
-    "database": os.environ.get("DB_NAME", "classservice"),
-}
+_pool = None
 
-connection_pool = pooling.MySQLConnectionPool(
-    pool_name="class_pool",
-    pool_size=5,
-    **db_config
-)
+def _get_pool():
+    global _pool
+    if _pool is None:
+        db_config = {
+            "host": os.environ.get("DB_HOST", "localhost"),
+            "port": int(os.environ.get("DB_PORT", 3306)),
+            "user": os.environ.get("DB_USER", "root"),
+            "password": os.environ.get("DB_PASSWORD", "password"),
+            "database": os.environ.get("DB_NAME", "classservice"),
+        }
+
+    _pool = pooling.MySQLConnectionPool(
+        pool_name="class_pool",
+        pool_size=5,
+        **db_config
+    )
+    return _pool
 
 def get_connection():
-    return connection_pool.get_connection()
+    return _get_pool.get_connection()
